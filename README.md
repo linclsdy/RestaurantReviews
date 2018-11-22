56937d3cd2d4e735abea578e55bfcc4fff10088b
linclsdy <40482603+linclsdy@users.noreply.github.com>
2018-11-22 12:26:16 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Restaurant Reviews App - Stage 1
+
+This project is a given restaurant review application of the Udacity Front-End Developer Nanodegree, which needs to refine the responsive design, accessiblity and offline availability. The given static webpage lacks accessibility in which it needs to convert into a mobile-base web application. The layout needs to be responsive on different displays and be accessible for screen readers. A service worker is added to create a seamless offline experience for users.
+
+
+## How to Run:
+
+1. Browse it [HERE](https://linclsdy.github.io/RestaurantReviews/) directly 
+
+2. or [Clone](https://github.com/linclsdy/RestaurantReviews.git) the repository
+* In this folder, start up a simple HTTP server to serve up the site files on your local computer. 
+* In a terminal, you can download [Python](https://www.python.org/) and install it. If you have Python 2.x, spin up the server with `python -m SimpleHTTPServer 8000`. For Python 3.x, you can use `python3 -m http.server 8000`.
+* Once the server is running, please go to browser and type `http://localhost:8000` on the search bar, then runs it.
+
+
---
+++ sw.js
@@ -0,0 +1,64 @@
+var staticCacheName = 'restaurant-v1';
+var cacheFiles = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('restaurant-v1').then(function(cache) {
+      return fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    })
+  );
+});
+
+/*
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(reponse) {
+
+      })
+    )
+  // TODO: respond to requests for the root page with
+  // the page skeleton from the cache
+  //Respond to requests for the root page with the page skeleton from the cache
+  var requestUrl = new URL(event.request.url);
+  
+  //Only intercept requests from the same origin (i.e. Don't intercept Google Fonts requests, or any third party request like that)
+  if (requestUrl.origin === location.origin) {
+    //Check if the request is for the root page
+    if (requestUrl.pathname === '/') {
+      //Respond with the cached skeleton, which will be there as it is now cached as part of the install step
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
+});
+*/
